5137d67cb3df4f48a9d6d7763cdc7d245aaa31a7
CS20B035 <100462069+CS20B035@users.noreply.github.com>
2023-02-22 15:59:05 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -78,6 +78,8 @@ def create_dataloaders(new_train_data, new_test_data, batch_size=64):
         print(f"X shape {X.shape}")
         print(f"y shape {y.shape}")
         break
+     
+    return train_dataloader, test_dataloader
 
 
 # model
